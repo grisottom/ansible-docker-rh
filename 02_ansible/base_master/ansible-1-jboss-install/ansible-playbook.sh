5cb57a608107f2e9664da9e61f773c9c9c0ebd51
Marcilio Grisotto <marcilio.grisotto@dataprev.gov.br>
2024-01-15 18:33:24 -0300

    Install Java. Control if jboss already installed

---
+++ 02_ansible/base_master/ansible-1-jboss-install/ansible-playbook.sh
@@ -0,0 +1,1 @@
+ansible-playbook -vvv -i inventory base.yml
---
+++ 02_ansible/base_master/docker-2-run-java-install.sh
@@ -0,0 +1,14 @@
+ANSIBLE_LOCAL_DIR=~/.ansible/
+
+docker run \
+  -it \
+  -h master_ansible \
+  -v ~/.ssh/master_ssh_key_pair/:/root/.ssh/ \
+  -v ./ansible-2-java-install:/ansible \
+  --mount type=bind,source=$ANSIBLE_LOCAL_DIR,target=/root/.ansible/ \
+  --rm --privileged \
+  --name=my_ansible_base_master \
+  --network=ansible-net \
+  ansible_base_master:latest \
+  sh ansible-playbook.sh
+
