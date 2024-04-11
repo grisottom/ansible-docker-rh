f3b7302dc1171890ea97a453dbe15eb335b3a504
marcilio.grisotto <marcilio.grisotto@dataprev.gov.br>
2024-04-11 16:13:46 -0300

    Hashi Vault, start of use

---
+++ 01_hosts/vault/docker-run.sh
@@ -0,0 +1,13 @@
+
+docker run \
+ -it \
+ -h base_vault \
+ --name base_vault \
+ --rm \
+ --cap-add=IPC_LOCK \
+ -e 'VAULT_DEV_ROOT_TOKEN_ID=vault_root_token' \
+ -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' \
+ -e 'VAULT_ADDR=http://0.0.0.0:8200' \
+ -e 'VAULT_TOKEN=vault_root_token' \
+ -p 8200:8200 \
+hashicorp/vault
